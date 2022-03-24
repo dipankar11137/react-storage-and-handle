a303d4527096f8e6f53aaf91c6f271c65a779950
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-25 00:09:37 +0600

    localhost

--- src/Components/Cosmatic/Cosmetic.js
+++ src/Components/Cosmatic/Cosmetic.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { addToDB, removeFromDb } from '../../Utilities/fakedb';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const { name, price, id } = props.cosmetic;
+
+    const addToCart = (id) => {
+        // console.log('item added', id);
+        // localStorage.setItem(id, 1);
+        addToDB(id);
+    }
+    // remove from cart
+    const removeFromCart = id => {
+        removeFromDb(id);
+    }
+
+    const addToCartWithParam = () => addToCart(id);
     return (
-        <div>
-            <hr />
-            <h1>Id : {id}</h1>
+        <div className='product'>
             <h2>Buy this : {name}</h2>
             <p>Only for : ${price}</p>
+            <p><small>It has id : {id}</small></p>
+
+            <button onClick={() => addToCart(id)}>Purchase</button>
+            <button onClick={() => removeFromCart(id)}>Remove</button>
         </div>
     );
 };
